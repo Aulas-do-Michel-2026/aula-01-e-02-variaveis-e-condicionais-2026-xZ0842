883faf34eba997d87bb3299f066a138351d4c81b
xZ0842 <enzocaveira@hotmail.com>
2026-04-01 16:30:40 -0300

    R_ex_1

--- exercicio_1.py
+++ exercicio_1.py
@@ -19,3 +19,11 @@ Dica: Use inputs para receber os dados!
 Lembre de converter ele para o tipo necessário!
 Print na tela com "print"
 """
+
+n1 = input('Digite a primeira nota: ')
+n2 = input('Digite a segunda nota: ')
+n3 = input('Digite a terceira nota: ')
+
+media = (float(n1) + float(n2) + float(n3)) / 3
+
+print(f"media do aluno: {media:.4f}")
