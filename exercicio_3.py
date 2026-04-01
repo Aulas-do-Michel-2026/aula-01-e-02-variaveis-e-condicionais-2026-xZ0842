4c28e2a7f95bcbb0f43ea3dc48b9d9bdcbbde552
xZ0842 <enzocaveira@hotmail.com>
2026-04-01 16:31:59 -0300

    R_ex_3
    Added functionality to check if a number is even or odd.

--- exercicio_3.py
+++ exercicio_3.py
@@ -17,3 +17,12 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+
+
+
+
+n = int(input("Digite um número: "))
+if n % 2 == 0:
+    print("Par")
+else:
+    print("Ímpar")
